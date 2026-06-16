7bb65752bc14a4a40e72bf6ff8997346181077d0
amantyagi9999 <amantyagi431@gmail.com>
2026-06-16 22:11:31 +0530

    Parking lot initial commit

---
+++ parking-lot/src/main/java/com/tutorial/sd/parkinglot/ParkingLotApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.sd.parkinglot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLotApplication.class, args);
+	}
+
+}
